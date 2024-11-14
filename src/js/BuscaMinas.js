722a4d1ad2147de6784b038415353664ccb47624
gabri <gabrielrl2004@gmail.com>
2024-11-14 21:33:36 +0100

    Getter of board added. Error of import fixed. Constructor modified. Method to generate a board created

--- src/js/BuscaMinas.js
+++ src/js/BuscaMinas.js
@@ -1,4 +1,4 @@
-import { Cell } from "./Cell";
+import { Cell } from "./Cell.js";
 
 /**
  * Clase que representa el juego del Buscaminas
@@ -8,13 +8,12 @@ export class BuscaMinas {
    * Crea un nuevo juego de Buscaminas
    * @param {number} columns - Representa el número de columnas
    * @param {number} rows - Representa el número de filas
-   * @param {number} bombsNumber - Representa el número de bombas
    */
-  constructor(columns, rows, bombsNumber) {
-    this.columns = columns;
+  constructor(rows, columns) {
     this.rows = rows;
-    this.bombsNumber = bombsNumber;
-    this.board = this.createBoard();
+    this.columns = columns;
+    this.bombsNumber = 0;
+    this.board = [];
   }
 
   /**
@@ -66,10 +65,18 @@ export class BuscaMinas {
   }
 
   /**
+   * Devuelve el array de celdas del tablero
+   * @returns {Cell[]} El tablero de celdas
+   */
+  getBoard() {
+    return this.board;
+  }
+
+  /**
    * Comprueba que una dimensión (fila o columna) cumple con lo indicado (menor o igual que 20 y mayor o igual que 10)
    * @returns {boolean} Si las dimensión cumple con los requisitos
    */
-  validDimension(dimension) {
+  static validDimension(dimension) {
     return dimension <= 20 && dimension >= 10;
   }
 
@@ -84,5 +91,81 @@ export class BuscaMinas {
     );
   }
 
-  createBoard() {}
+  generateBoard() {
+    //Generamos un array bidimensional de celdas con las dimensiones especificadas
+    let board = [];
+    let counter = this.bombsNumber;
+    for (let i = 0; i < this.rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        board[i][j] = new Cell(i, j, false);
+      }
+    }
+
+    //Repartimos las bombas de forma aleatoria por el tablero
+    do {
+      let row = parseInt(Math.random() * this.rows);
+      let column = parseInt(Math.random() * this.columns);
+
+      if (!board[row][column].getHasBomb()) {
+        board[row][column].setHasBomb(true);
+        counter--;
+      }
+    } while (counter > 0);
+
+    //Constante con las diferentes celdas a revisar
+    const directions = [
+      [1, -1], //Diagonal superior izquierda
+      [1, 0], //Central superior
+      [1, 1], //Diagonal superior derecha
+      [0, -1], //Central izquierda
+      [0, 1], //Central derecha
+      [-1, -1], //Diagonal inferior izquierda
+      [-1, 0], //Central inferior
+      [-1, 1], //Diagonal inferior derecha
+    ];
+
+    //Revisar y asignar el numero de bombas cercanas
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        let nearBombs = 0;
+        for (let [x, y] of directions) {
+          let nearRow = r + x;
+          let nearColumn = c + y;
+          if (
+            nearRow >= 0 &&
+            nearRow < this.rows &&
+            nearColumn >= 0 &&
+            nearColumn < this.columns
+          ) {
+            if (board[nearRow][nearColumn].getHasBomb()) {
+              nearBombs++;
+            }
+          }
+        }
+        board[r][c].setNearBombs(nearBombs);
+      }
+    }
+
+    //Guardamos el tablero creado
+    this.board = board;
+  }
+
+  printBoard() {
+    let board = "";
+
+    for (let r = 0; r < this.rows; r++) {
+      board += "<tr>";
+      for (let c = 0; c < this.columns; c++) {
+        board += `<td>${
+          this.board[r][c].getHasBomb()
+            ? "Mina"
+            : this.board[r][c].getNearBombs()
+        }</td>`;
+      }
+      board += "</tr>";
+    }
+
+    return board;
+  }
 }
